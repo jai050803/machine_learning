343b0b96de5ce3c272859bf41ecf0f7b88bfab38
jai <sjai5803@gmail.com>
2024-04-07 13:22:35 +0530

    sales forecasting with flask and machine learning

--- machine_learning_projects/sales_forecasting/FLASK/app.py
+++ machine_learning_projects/sales_forecasting/FLASK/app.py
@@ -1,7 +1,33 @@
 #import necessary libraries
 import numpy as np
 import pandas as pd
-from flask import flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = flask(__name__)
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+
+@app.route('/results', methods=['POST'])
+def results():
+    data = request.get_json(force=True)
+    predict =model.predict([np.array(list(data.values()))])
+    output = predict[0]
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
